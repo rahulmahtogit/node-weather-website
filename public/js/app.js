d25bc5069d7c261a48015b4ac58b70e2b81e37d3
rahulmahtogit <rahul21051995@gmail.com>
2020-09-18 04:28:18 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageTwo.textContent = ""
   
 
-fetch(`http://localhost:3000/weather?address=${location}`,
+fetch(`/weather?address=${location}`,
 ).then((response) =>{
     response.json().then(data=>{
         
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define path for Express Configuration
 const publicDirectoryPath = path.join(__dirname,"../public")
@@ -112,6 +113,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is up on 3000")
+app.listen(port,()=>{
+    console.log("Server is up on " + port)
 })
